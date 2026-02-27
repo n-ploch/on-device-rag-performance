4895f07a1405050e4fc256b7b986f4cad2bd6d0f
n-ploch <noah.ploch@cdtm.de>
2026-02-27 20:10:18 +0100

    make langfuse exporter selectable

--- orchestrator/src/orchestrator/exporters/__init__.py
+++ orchestrator/src/orchestrator/exporters/__init__.py
@@ -2,10 +2,12 @@
 
 from orchestrator.exporters.factory import create_exporter
 from orchestrator.exporters.jsonl import JSONLSpanExporter
+from orchestrator.exporters.langfuse_exporter import LangfuseExporter
 from orchestrator.exporters.span_converter import EvaluationSpan, result_to_span
 
 __all__ = [
     "JSONLSpanExporter",
+    "LangfuseExporter",
     "create_exporter",
     "result_to_span",
     "EvaluationSpan",
